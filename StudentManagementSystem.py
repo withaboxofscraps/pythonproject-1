7dd2a43deb3130855eab947215a56b6a46bc1642
sj-builds-911 <sanjay782757@gmail.com>
2026-06-30 10:38:59 +0530

    student management system updated

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -1,76 +1,76 @@
 import json
 try:
-    with open("managementsystem.json","r") as f:
+    with open("tryagainn.json")as f:
         lst=json.load(f)
 except:
     lst=[]
-
-stu={
-    'name': '',
-    'age': '',
-    'grade': ''
- }
-def addstudent():
-    stu['name']=input("Enter student name:")
-    stu['age']=input("Enter student age:")
-    stu['grade']=input("Enter student grade:")
-    lst.append(stu.copy())
-    with open("managementsystem.json","w") as f:
+student={
+    'name':'',
+    'age':'',
+    'grade':''
+    }
+def adddata():
+    student['name']=input("enter the name of the student: ")
+    student['age']=input("enter the age of the student: ")
+    student['grade']=input("what grade is he/she in? ")
+    lst.append(student.copy())
+    print("data added successfully")
+    with open("tryagain.json","w") as f:
         json.dump(lst,f)
-    print('successfully')
-
-def search():
-    srh=input("what do you want to search?")
-    for x in lst:
-     if srh == x['name']:
-      print(f"{x['name']}\t{x['age']}\t{x['grade']}")
 
 def update():
-    req=input("what do you want to update?")
-    for y in lst:
-     if req == "name":
-         req_2=input('whose name?')
-         if req_2 == y['name']:
-          y['name']=input("enrter new name")
-         with open("managementsystem.json","w") as f:
-            json.dump(lst,f)
+    name=input("whose data you wan't to change? ")
+    olddata=input(f"what do you want to change?")
+    newdata=input("enter the new data")
+    for i in lst:
+      if i['name']==name:
+          i[olddata]=newdata
+   
+    with open("tryagain.json","w") as f:
+      json.dump(lst,f)
 
-def delete():
-    wasp = input("Whose data you want to delete? ")
 
-    for a in lst:
-      if wasp == a['name']:
-        lst.remove(a)
-        print("Deleted")
-        break
-    with open("managementsystem.json","w") as f:
+def deletedata():
+    jay=input("what do you want to delete? ")
+    for j in lst:
+        if j['name']==jay:
+            lst.remove(j)
+    with open("tryagain.json","w") as f:
         json.dump(lst,f)
 
 def view():
     for i in lst:
-     print(f"{i['name']}\t{i['age']}\t{i['grade']}")
-    
+      print(f"{i['name']}\t{i['age']}\t{i['grade']}")
 
-print("---STUDENT MANAGEMENT SYSTEM---\n"
-      "1. Add student\n"
-      "2. View student\n"
-      "3.search\n"
-      "4.updatee\n"
-      "5.delete")
+def search():
 
+    jk=input("whose data you want to delete? ")
+
+    for k in lst:
+        if k['name']==jk:
+            print(f"{k['name']}\t{k['age']}\t{k['grade']}")
+
+
+
+print("-------STUDENT MANAGEMENT SYSTEM-------\n",
+      "1-add data\n"
+      "2-delete data\n"
+      "3-search data\n"
+      "4-update data\n"
+      "5-view data")
 
 while True:
-
- get_choice=input("what we doing today?")
- if get_choice=="1":
-    addstudent()
- elif get_choice=="2":
-    view()
- elif get_choice=="3":
-     search()
- elif get_choice=="4":
-     update()
- elif get_choice=="5":
-     delete()
- else:
-    break
+    ans=input("what we doing today bro?")
+    if ans=='1':
+       adddata()
+    elif ans=='2':
+       deletedata()
+    elif ans=='3':
+       search()
+    elif ans=='4':
+       update()
+    elif ans=='5':
+       view()
+    else:
+       print("thank you broskies")
+       break
